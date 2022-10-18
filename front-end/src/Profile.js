bf102fe7605cceb1f8163124f9e5f1378a710557
laura <qlaueen@gmail.com>
2022-10-18 16:15:54 -0400

    created header component

---
+++ front-end/src/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Components/Header';
+
+
+export default function Profile() {
+  return (
+    
+    <div className="profile-page-container">
+      <Header />
+      <div className="header-profile-page">
+        
+      </div>
+    </div>
+  )
+}
