b34507aa53b00e56d474401c4c4de3901709f353
Phil Wee <pkw2010@nyu.edu>
2022-10-24 19:24:46 -0400

    ship sign in

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,17 +1,38 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function (props) {
+  let [loginMode, setloginMode] = useState("signin")
+
+  const changeloginMode = () => {
+    setloginMode(loginMode === "signin" ? "signup" : "signin")
+  }
+
+
   return (
-    <div className="login-page-container">
-      <form className="login-page">
-        <div className="login-page-content">
-          <h3 className="login-page-title">Sign In</h3>
+    <div className="login-form-container">
+      <form className="login-form">
+        <div className="login-form-content">
+          <h3 className="login-form-title">Sign In</h3>
+          <div className="text-center">
+            Already registered?{" "}
+            <span className="link-primary" onClick={changeloginMode}>
+              Sign In
+            </span>
+          </div>
+          <div className="form-group mt-3">
+            <label>Full Name</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="e.g Jane Doe"
+            />
+          </div>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
               type="email"
               className="form-control mt-1"
-              placeholder="Enter email"
+              placeholder="Email Address"
             />
           </div>
           <div className="form-group mt-3">
@@ -19,19 +40,63 @@ export default function (props) {
             <input
               type="password"
               className="form-control mt-1"
-              placeholder="Enter password"
+              placeholder="Password"
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="login" className="btn btn-primary">
-              Login
+            <button type="submit" className="btn btn-primary">
+              Submit
             </button>
           </div>
-          <p className="forgot-password text-right mt-2">
-          <a href="#"> Forgot password?</a>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
           </p>
         </div>
       </form>
     </div>
   )
+
+  if (loginMode === "signin") {
+    return (
+      <div className="login-form-container">
+        <form className="login-form">
+          <div className="login-form-content">
+            <h3 className="login-form-title">Sign In</h3>
+            <div className="text-center">
+              Not registered yet?{" "}
+              <span className="link-primary" onClick={changeloginMode}>
+                Sign Up
+              </span>
+            </div>
+            <div className="form-group mt-3">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control mt-1"
+                placeholder="Enter email"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+            <p className="text-center mt-2">
+            <a href="#">Forgot your password?</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    )
+  }
+
+
 }
