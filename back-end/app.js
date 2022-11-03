8f5c5444ed26a3aa707090b2ca01bc1c6c9bf863
raa573 <raa573@nyu.edu>
2022-11-03 12:52:53 -0400

    Back-end installation

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+// import and instantiate express
+const express = require("express") // CommonJS import style!
+const app = express() // instantiate an Express object
+// we will put some server logic here later...
+// Routes
+// sample code
+app.use('/', (req, res, next) => {
+    const filters = req.query;
+    const filteredUsers = data.filter(user => {
+      let isValid = true;
+      for (key in filters) {
+        console.log(key, user[key], filters[key]);
+        isValid = isValid && user[key] == filters[key];
+      }
+      return isValid;
+    });
+    res.send(filteredUsers);
+  });
+// end of sample code
+// export the express app we created to make it available to other modules
+module.exports = app
