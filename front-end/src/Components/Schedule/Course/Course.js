3e235fd5bd907a9b91799321473d92f7884e6d61
Yuri <yurijacq@gmail.com>
2022-11-14 14:51:16 -0500

    working with delete button and pop ups

--- front-end/src/Components/Schedule/Course/Course.js
+++ front-end/src/Components/Schedule/Course/Course.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import { useState, useEffect } from 'react'
-import { getUsers, getCourses } from '../../../actions/courses'
-import { useSelector } from 'react-redux';
+import Schedule from '../Schedule';
+import { removeFromSchedule } from '../../../api';
+import RemoveClassPopup from '../../RemoveClassPopup'
+
 
 
 const Course = (props) => {
-  
   const schedule = props.schedule
+
+  const handleRemove= (section) =>{
+    // const userid ="636ed77cfe63d7bf6b544a93"
+    // console.log(section.id)
+    removeFromSchedule(section.id)
+    Schedule()
+    RemoveClassPopup()
+  }
+
+
 
   return (
     <>
@@ -23,7 +33,7 @@ const Course = (props) => {
                 <p>{section.prof}</p>
                 <p>{section.time}</p>
                 <p>{section.loc}</p>
-                <Button>Remove from schedule</Button>
+                <Button variant="primary" onClick= {() => handleRemove(section)}>Remove from Schedule</Button>)
               </Card.Text>
             </Card.Body>
           </Card>
