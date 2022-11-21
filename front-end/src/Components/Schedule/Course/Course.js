fcbbb0ed2b470ed1937759661fdd6612df87f8b1
Yuri <yurijacq@gmail.com>
2022-11-21 03:49:15 -0500

    conflict alert

--- front-end/src/Components/Schedule/Course/Course.js
+++ front-end/src/Components/Schedule/Course/Course.js
@@ -1,15 +1,18 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Schedule from '../Schedule';
 import { removeFromSchedule } from '../../../api';
 import RemoveClassPopup from '../../RemoveClassPopup'
+import { changeSection } from '../../../actions/courses';
 
 
 
 const Course = (props) => {
-  const schedule = props.schedule
+  let schedule = props.schedule
   const [visible, setVisible] = useState(true)
+
+
   const handleRemove = (section) =>{
     // const userid ="636ed77cfe63d7bf6b544a93"
     // console.log(section.id)
@@ -19,6 +22,27 @@ const Course = (props) => {
   }
 
 
+  const isConflicting = () => {
+    let prev = {}
+    schedule.map(section => {
+      // section.conflicting = false
+      if (prev.time === section.time){
+        prev.conflicting = true
+        section.conflicting = true
+      }
+      prev=section
+    })
+  }
+
+
+  const AlertConflict = (props) => {
+    const conflicting= props.conflicting
+    if (conflicting){
+      return ("WARNING: time conflict with other classes")
+    }
+  }
+
+  isConflicting()
 
   return (
     <>
@@ -33,6 +57,7 @@ const Course = (props) => {
               {visible && <p>{section.prof}</p> }
               {visible &&  <p>{section.time}</p> }
               {visible &&  <p>{section.loc}</p>}
+              {visible &&  <p id="warning"> <AlertConflict conflicting={section.conflicting}/> </p>}
               </Card.Text>
               {visible && <Button variant="primary" onClick= {() => handleRemove(section)}>Remove from Schedule</Button>}
             </Card.Body>
