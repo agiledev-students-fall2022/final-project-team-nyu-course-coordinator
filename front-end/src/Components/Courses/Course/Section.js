95d466e8218f631fa8e4d0598a8d5d45f732b31a
raa573 <raa573@nyu.edu>
2022-12-07 15:09:55 -0500

    Bug fixes

--- front-end/src/Components/Courses/Course/Section.js
+++ front-end/src/Components/Courses/Course/Section.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
-import  { getUsers }  from '../../../actions/courses.js'; 
+// import  { getUsers }  from '../../../actions/courses.js'; 
 import AddClassPopup from './AddClassPopup'
 
 const Section = (session) => { 
--- front-end/src/Components/Schedule/Course/Course.js
+++ front-end/src/Components/Schedule/Course/Course.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Card from 'react-bootstrap/Card';
 import RemoveClassPopup from './RemoveClassPopup'
 
