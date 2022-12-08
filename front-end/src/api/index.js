36d0b65ede4ad91931a9e46c81f40682bafe26c9
raa573 <raa573@nyu.edu>
2022-12-08 12:39:22 -0500

    .env fix

--- front-end/src/api/index.js
+++ front-end/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_SERVER_HOSTNAME,
 });
 
 api.interceptors.request.use((req) => {
